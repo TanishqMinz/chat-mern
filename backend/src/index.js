61bceb1870a3d91c7d104508d5f2fbcb6b549a69
Tanishq <tanishqminz@gmail.com>
2025-03-24 16:41:39 +0530

    feat: Message Routes added(socket.io integration pending)

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser'
 
-import authroutes from "./routes/auth.route.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config()
@@ -14,7 +15,8 @@ app.use(cookieParser())
 
 const PORT = process.env.PORT || 5001
 
-app.use("/api/auth", authroutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
