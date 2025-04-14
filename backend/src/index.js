fb224263fcb914eb9a90e2c6c2a46bcd0697d27e
Tanishq <tanishqminz@gmail.com>
2025-04-14 13:08:34 +0530

    Changes to setup a production environment

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser'
 import cors from "cors"
 
+import path from "path"
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
@@ -23,9 +25,18 @@ app.use(cors({
 )
 
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+
+if(process.env.NODE_ENV==="production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 
 // remember to change baseURL after deploying
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api": "/api",
     withCredentials: true
 })
